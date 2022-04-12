a5225675c41fd7753e6fb873bd36a90637741639
Mauricio Torres <otarksk@gmail.com>
2022-04-12 15:09:37 -0300

    Version 8

--- src/main/java/com/example/demo/service/impl/UsuarioServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UsuarioServiceImpl.java
@@ -4,14 +4,12 @@ import com.example.demo.domain.Usuario;
 import com.example.demo.repository.UsuarioRepo;
 import com.example.demo.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 @Service
 public class UsuarioServiceImpl implements UsuarioService {
     @Autowired
-    @Qualifier("user")
     private UsuarioRepo usuarioRepo;
 
     @Override
